19065bfc50c62b689ff581b66eeafc64535e6c0a
Miyakawa Takuya <omyakawa@gmail.com>
2021-06-17 12:09:52 +0900

    Initial Commit

---
+++ PhotoLife/PhotoLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLifeApp.swift
+//  PhotoLife
+//
+//  Created by 宮川卓也 on 2021/06/17.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
